00cfe67410b1ba4aea3a72ef9128d1d4d5e58e85
Evenie <eveniehorton@gmail.com>
2014-11-10 20:18:40 -0600

    Merged two projects into one

--- Weapon.java
+++ Weapon.java
@@ -1,57 +1,27 @@
 package RPG;
 
 public class Weapon {
-<<<<<<< HEAD
-	
-	// Instance variable
-	private int damage;
-	private String name;
-	
-	// Type constants
-	public enum Type {SWORD, EXPLOSIVE, PROJECTILE, WAND};
-	
-	public Weapon(Type type, String name) {
-		if (type == Type.EXPLOSIVE) {
-			damage = 3;
-		}
-		else if (type == Type.PROJECTILE) {
-			damage = 1;
-		}
-		else {
-			damage = 2;
-=======
 
  	private int damage;
 	private String name;
-	public enum Group {EXPLOSIVE, BLADE, FOOD, MAGIC, PROJECTILE};
+	public enum Type {EXPLOSIVE, BLADE, FOOD, MAGIC, PROJECTILE};
 	
-	public Weapon(Group type, String name) {
-		if(type == Group.EXPLOSIVE) {
+	public Weapon(Type type, String name) {
+		if(type == Type.EXPLOSIVE) {
 			damage = 4;
 		}
-		else if(type == Group.BLADE) {
+		else if(type == Type.BLADE) {
 			damage = 3;
 		}
-		else if(type == Group.MAGIC) {
+		else if(type == Type.MAGIC) {
 			damage = 2;
 		}
 		else {
 			damage = 1;
->>>>>>> 855915d7b3d51bd16c5a6ebe74b23d3dc30c8776
 		}
 		this.name = name;
 	}
-	
-<<<<<<< HEAD
-	public int damage() {
-		return damage;
-	}
-	
-	public String name() {
-		return name;
-	}
 
-=======
 	public String name() {
 		return name;
 	}
@@ -59,6 +29,5 @@ public class Weapon {
 	public int damage() {
 		return damage;
 	}
->>>>>>> 855915d7b3d51bd16c5a6ebe74b23d3dc30c8776
 
 }
