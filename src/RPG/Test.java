b5c5a81cf92e7e62042d4cc114d0fd2625f8fecc
Gregory Theriot <gtheriot@uno.edu>
2014-12-01 16:32:21 -0600

    minor edits
    nothing huge.

--- src/RPG/Test.java
+++ src/RPG/Test.java
@@ -1,9 +1,35 @@
 package RPG;
 
+import java.util.ArrayList;
+
+import RPG.Rooms.TestRoom;
+
 public class Test {
 
 	public static void main(String[] args) {
+		
+		Room r=new TestRoom();
+		ArrayList<BoardObject> stuff=r.getObjects();
+		ArrayList<Mob> mobs= new ArrayList<Mob>();
 		BoardObject a= new PlayerCharacter(4,2, "Joseph", WeaponCreator.createWeapon());
+		BoardObject[] boardarray={new PlayerCharacter(1,1,"a",WeaponCreator.createWeapon())};
+		r.setObjects((PlayerCharacter)a, boardarray);
+		
+		for(int i=0;i<stuff.size();i++){
+			if(stuff.get(i) instanceof Mob){
+				mobs.add((Mob)stuff.get(i));
+			}
+		}
+		
+		mobs.get(0).takeDamage(10);
+		
+		for(int i=0;i<stuff.size();i++){
+			if(mobs.get(0).equals(stuff.get(i))){
+				System.out.println("They match!");
+			}
+		}
+		
+		
 		System.out.println(a instanceof PlayerCharacter);
 
 	}
--- src/sandbox/Test2.java
+++ src/sandbox/Test2.java
@@ -1,13 +1,9 @@
 package sandbox;
 
+
 public class Test2 {
 	
 	public static void main(String[] args){
-		
-		StringBuilder[] array={new StringBuilder("a"),new StringBuilder("b"),new StringBuilder("c"),new StringBuilder("d")};
-		
-		StringBuilder a = array[0];
-		
 		
 		
 		
